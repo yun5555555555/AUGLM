68fd02fcea0bf77011226b055f09a814d7ecc70d
yun5555555555 <3990117600@qq.com>
2025-04-29 22:23:35 +0800

    Add files via upload

--- AUG重制版.lua
+++ AUG重制版.lua
@@ -70,196 +70,58 @@ about:Label("你现在的服务器id:"..game.GameId)
 about:Label("你的用户ID:"..game.Players.LocalPlayer.UserId)
 about:Label("获取客户端ID:"..game:GetService("RbxAnalyticsService"):GetClientId())
 
+local UITab3 = win:Tab("各大脚本",'16060333448')
+
+local about = UITab3:section("各大脚本",true)
+
+about:Label("你们知道为什么第一个不是通用而是各大脚本吗")
+about:Label("因为自己的不行")
+
+about:Button("皮脚本",function()
+getgenv().XiaoPi="皮脚本QQ群894995244" loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaopi77/xiaopi77/main/QQ1002100032-Roblox-Pi-script.lua"))()
+end)
+
+about:Button("皮脚本ohio",function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/pijiaobenMSJMleng/ehhdvdhd/refs/heads/main/xiaopi77xiaopi77mainQQ1002100032-Roblox-Pi-script.lua"))()
+end)
+
+about:Button("皮脚本河北",function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaopi77/xiaopi77/main/Pi-script-Hebeitangxian.lua"))()
+end)
+
+about:Button("云脚本",function()
+loadstring(game:HttpGet("https://github.com/CloudX-ScriptsWane/White-ash-script/raw/main/Beta.lua", true))()
+end)
+
+about:Button("鬼脚本",function()
+Ghost_Script = "作者_鬼"Ghost_Script = "交流群858895377"loadstring(game:HttpGet("https://raw.githubusercontent.com/Ghost-Gui-888/Ghost-Script/refs/heads/main/QQ858895377"))()
+end)
+
+about:Button("苏脚本",function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kukunb/kukulol/refs/heads/main/SUscript.txt"))();
+end)
+
+about:Button("叶主脚本",function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/roblox-ye/QQ515966991/refs/heads/main/ROBLOX-CNVIP-XIAOYE.lua"))()
+end)
+
+about:Button("叶俄亥俄州脚本",function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/roblox-ye/QQ515966991/refs/heads/main/YE-%20Scripts-OHIO.lua"))()
+end)
+
+about:Button("叶河北唐县脚本",function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/roblox-ye/QQ515966991/refs/heads/main/YE%20SCRIPT-Tang%20County%2C%20Hebei.lua"))()
+end)
+
+about:Button("宿谈脚本",function()
+loadstring(game:HttpGet(string.char(104, 116, 116, 112, 115, 58, 47, 47, 114, 97, 119, 46, 103, 105, 116, 104, 117, 98, 117, 115, 101, 114, 99, 111, 110, 116, 101, 110, 116, 46, 99, 111, 109, 47, 83, 117, 107, 117, 110, 97, 50, 49, 51, 52, 47, 45, 47, 114, 101, 102, 115, 47, 104, 101, 97, 100, 115, 47, 109, 97, 105, 110, 47, 37, 69, 53, 37, 65, 69, 37, 66, 70, 37, 69, 53, 37, 56, 50, 37, 65, 57, 37, 69, 56, 37, 56, 52, 37, 57, 65, 37, 69, 54, 37, 57, 67, 37, 65, 67, 50)))()
+end)
+
+about:Button("神经脚本",function()
+loadstring(game:HttpGet("https://github.com/shenjin55/qq-927274889/raw/refs/heads/main/roblox-shen-jin.lua"))()
+end)
+
 local UITab3 = win:Tab("通用",'16060333448')
 
 local about = UITab3:section("通用",true)
 
-about:Button("玩家加入游戏提示",function()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/boyscp/scriscriptsc/main/bbn.lua"))()
-end)
-
-about:Button("获得管理员权限",function()
-loadstring(game:HttpGet("https://pastebin.com/raw/sZpgTVas"))()
-end)
-
-about:Button("死亡笔记",function()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/dingding123hhh/tt/main/%E6%AD%BB%E4%BA%A1%E7%AC%94%E8%AE%B0%20(1).txt"))()
-end)
-
-about:Button("汉化穿墙",function()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/TtmScripter/OtherScript/main/Noclip"))()
-end)
-    
-about:Button("叶飞行",function()
-loadstring(game:HttpGet("https://pastebin.com/raw/jeaenuuK"))()
-end)
-
-about:Button("透视1",function()
-loadstring(game:HttpGet('https://pastebin.com/raw/MA8jhPWT'))()
-end)
-
-about:Button("飞檐走壁",function()
-    loadstring(game:HttpGet("https://pastebin.com/raw/zXk4Rq2r"))()
-end)
-
-about:Button("踏空行走",function()
-loadstring(game:HttpGet('https://raw.githubusercontent.com/GhostPlayer352/Test4/main/Float'))()
-end)
-
-about:Button("电脑键盘",function()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/advxzivhsjjdhxhsidifvsh/mobkeyboard/main/main.txt", true))()
-end)
-
-about:Button("无敌",function()
-loadstring(game:HttpGet('https://pastebin.com/raw/H3RLCWWZ'))()
-end)
-
-about:Button("隐身",function()
-loadstring(game:HttpGet('https://pastebin.com/raw/nwGEvkez'))()
-end)
-
-about:Button("iw指令",function()
-  loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()
-end)
-
-about:Toggle("无限跳","Toggle",false,function(Value)
-        Jump = Value
-        game.UserInputService.JumpRequest:Connect(function()
-            if Jump then
-                game.Players.LocalPlayer.Character.Humanoid:ChangeState("Jumping")
-            end
-        end)
-    end)
-
-about:Toggle("夜视","Toggle",false,function(Value)
-if Value then
-
-		    game.Lighting.Ambient = Color3.new(1, 1, 1)
-
-		else
-
-		    game.Lighting.Ambient = Color3.new(0, 0, 0)
-
-		end
-end)
-
-about:Button("透视",function()  
-    _G.FriendColor = Color3.fromRGB(0, 0, 255)
-        local function ApplyESP(v)
-       if v.Character and v.Character:FindFirstChildOfClass'Humanoid' then
-           v.Character.Humanoid.NameDisplayDistance = 9e9
-           v.Character.Humanoid.NameOcclusion = "NoOcclusion"
-           v.Character.Humanoid.HealthDisplayDistance = 9e9
-           v.Character.Humanoid.HealthDisplayType = "AlwaysOn"
-           v.Character.Humanoid.Health = v.Character.Humanoid.Health -- triggers changed
-       end
-    end
-    for i,v in pairs(game.Players:GetPlayers()) do
-       ApplyESP(v)
-       v.CharacterAdded:Connect(function()
-           task.wait(0.33)
-           ApplyESP(v)
-       end)
-    end
-    
-    game.Players.PlayerAdded:Connect(function(v)
-       ApplyESP(v)
-       v.CharacterAdded:Connect(function()
-           task.wait(0.33)
-           ApplyESP(v)
-       end)
-    end)
-    
-        local Players = game:GetService("Players"):GetChildren()
-    local RunService = game:GetService("RunService")
-    local highlight = Instance.new("Highlight")
-    highlight.Name = "Highlight"
-    
-    for i, v in pairs(Players) do
-        repeat wait() until v.Character
-        if not v.Character:FindFirstChild("HumanoidRootPart"):FindFirstChild("Highlight") then
-            local highlightClone = highlight:Clone()
-            highlightClone.Adornee = v.Character
-            highlightClone.Parent = v.Character:FindFirstChild("HumanoidRootPart")
-            highlightClone.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
-            highlightClone.Name = "Highlight"
-        end
-    end
-    
-    game.Players.PlayerAdded:Connect(function(player)
-        repeat wait() until player.Character
-        if not player.Character:FindFirstChild("HumanoidRootPart"):FindFirstChild("Highlight") then
-            local highlightClone = highlight:Clone()
-            highlightClone.Adornee = player.Character
-            highlightClone.Parent = player.Character:FindFirstChild("HumanoidRootPart")
-            highlightClone.Name = "Highlight"
-        end
-    end)
-    
-    game.Players.PlayerRemoving:Connect(function(playerRemoved)
-        playerRemoved.Character:FindFirstChild("HumanoidRootPart").Highlight:Destroy()
-    end)
-    
-    RunService.Heartbeat:Connect(function()
-        for i, v in pairs(Players) do
-            repeat wait() until v.Character
-            if not v.Character:FindFirstChild("HumanoidRootPart"):FindFirstChild("Highlight") then
-                local highlightClone = highlight:Clone()
-                highlightClone.Adornee = v.Character
-                highlightClone.Parent = v.Character:FindFirstChild("HumanoidRootPart")
-                highlightClone.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
-                highlightClone.Name = "Highlight"
-                task.wait()
-            end
-    end
-    end)
-    end)
-    
-    about:Button("汉化穿墙",function()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/TtmScripter/OtherScript/main/Noclip"))()
-end)
-
-about:Button("替身",function()
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/SkrillexMe/SkrillexLoader/main/SkrillexLoadMain')))()
-end)
-
-about:Button("爬墙",function()
-loadstring(game:HttpGet("https://pastebin.com/raw/zXk4Rq2r"))()
-end)
-
-about:Button("甩人",function()
-loadstring(game:HttpGet("https://pastebin.com/raw/zqyDSUWX"))()
-end)
-about:Slider('设置重力', 'Sliderflag', 196.2, 196.2, 1000,false, function(Value)
-        game.Workspace.Gravity = Value
-    end)
-
-about:Slider("步行速度!", "WalkSpeed", game.Players.LocalPlayer.Character.Humanoid.WalkSpeed, 16, 400, false, function(Speed)
-  spawn(function() while task.wait() do game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Speed end end)
-end)
-
-about:Slider("跳跃高度!", "JumpPower", game.Players.LocalPlayer.Character.Humanoid.JumpPower, 50, 400, false, function(Jump)
-  spawn(function() while task.wait() do game.Players.LocalPlayer.Character.Humanoid.JumpPower = Jump end end)
-end)
-
-about:Toggle("ESP 显示名字", "AMG", ENABLED, function(enabled)
-    if enabled then ENABLED = true for _, player in ipairs(Players:GetPlayers()) do onPlayerAdded(player) end Players.PlayerAdded:Connect(onPlayerAdded) Players.PlayerRemoving:Connect(onPlayerRemoving) local localPlayer = Players.LocalPlayer if localPlayer and localPlayer.Character then for _, player in ipairs(Players:GetPlayers()) do if player.Character then createNameLabel(player) end end end RunService.Heartbeat:Connect(function() if ENABLED then for _, player in ipairs(Players:GetPlayers()) do if player.Character then createNameLabel(player) end end end end) else ENABLED = false for _, player in ipairs(Players:GetPlayers()) do onPlayerRemoving(player) end RunService:UnbindFromRenderStep("move") end
-end)
-
-local UITab1 = win:Tab("音乐",'7734068321')
-
-local about = UITab1:section("音乐",true)
-
-about:Button("防空警报", function()
-    local sound = Instance.new("Sound")
-    sound.SoundId = "rbxassetid://792323017"
-    sound.Parent = game.Workspace
-    sound:Play()
-    end)
-    
-about:Button("义勇军进行曲", function()
-    local sound = Instance.new("Sound")
-    sound.SoundId = "rbxassetid://1845918434"
-    sound.Parent = game.Workspace
-    sound:Play()
-    end)
